52d854ee5c267263742b003171afcdd5f09152fc
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-08-31 15:30:00 -0500

    Create readme.ml

---
+++ readme.ml
@@ -0,0 +1,3 @@
+Authors: Preston Brubaker, Willoh Brubaker
+The most recently added version of this program is two_brains.py.
+See an explination for this simulation at https://markoviandevelopments.com/articles/mars_quest/mars_quest.html
